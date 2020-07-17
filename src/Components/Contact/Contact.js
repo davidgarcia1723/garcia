ff7632a1f69da86e1711597034448abc38322102
David Garcia <davidgarcia1723@gmail.com>
2020-07-17 17:25:54 -0400

    email submission

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,120 +1,125 @@
+
 import React from 'react';
 import '../Contact/Contact.css';
 
-const initialState = {
-    name: "",
-    email: "",
-    message: "",
-    nameError: "",
-    emailError: "",
-    messageError: ""
-  };
-  
-  export default class Contact extends React.Component {
-    state = initialState;
-  
-    handleChange = event => {
-      const isCheckbox = event.target.type === "checkbox";
-      this.setState({
-        [event.target.name]: isCheckbox
-          ? event.target.checked
-          : event.target.value
-      });
-    };
-  
-    validate = () => {
-      let nameError = "";
-      let emailError = "";
-      let messageError = "";
-  
-      if (!this.state.name) {
-        nameError = "Name cannot be blank";
-      }
-  
-      if (!this.state.email.includes("@")) {
-        emailError = "Invalid email.";
-      }
 
-      if (!this.state.message) {
-        messageError = "Message is required.";
-      }
-  
-      if (emailError || nameError || messageError) {
-        this.setState({ emailError, nameError, messageError });
-        return false;
-      }
-  
-      return true;
+const initialState = {
+    Name: '',
+    Email: '',
+    Message: '',
+    errorName: '',
+    errorEmail: '',
+    errorMessage: ''
+};
+
+class Contact extends React.Component {
+    state = initialState;
+
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+            [event.target.name]: isCheckbox
+                ? event.target.checked
+                : event.target.value
+        });
     };
-  
-    handleSubmit = event => {
-      event.preventDefault();
-      const isValid = this.validate();
-      if (isValid) {
-        console.log(this.state);
-        // clear form
-        this.setState(initialState);
-      }
-    };
-  
-    render() {
-      return (
-          <div>
-            <div>
-            <div class="hero-image--contact">
-                    <div class="hero-text--contact">
-                        <h1>Contact Me</h1>
-                        <p>Let's Connect.</p>
-                        <br />
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-                 </div>
-        <form className="container-form" action="https://formsubmit.co/davidgarcia1723@gmail.com" method="POST" onSubmit={this.handleSubmit}> 
-            <label for="name">Name:</label>
-          <div>
-            <input
-              name="name"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <div style={{ fontSize: 12, color: "red" }}>
-              {this.state.nameError}
-            </div>
-          </div>
-          <br />
-          <label for="email">Email:</label>
-          <div>
-            <input
-              name="email"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-            <div style={{ fontSize: 12, color: "red" }}>
-              {this.state.emailError}
-            </div>
-          </div>
-          <br />
-          <label for="message">Enter Message Here:</label>
-          <div> 
-            <textarea 
-              cols="50" rows="10"  name="message"
-              placeholder="Enter message here."
-              value={this.state.message}
-              onChange={this.handleChange}>
-            </textarea>
-            <div style={{ fontSize: 12, color: "red" }}>
-              {this.state.messageError}
-            </div>
-          </div>
-          <br />
-          <button type="submit" className="btn-submit">Submit</button>
-        </form>
-        </div>
-         );
+
+    validate = (event) => {
+        let errorName = '';
+        let errorEmail = '';
+        let errorMessage = '';
+
+        if (this.state.Name.length < 3) {
+            errorName = "Please Enter Name";
         }
-      }
+        if (errorName) {
+            this.setState({ errorName });
+            return false
+        }
+        if (this.state.Email.indexOf('@') === -1 || this.state.Email.length < 6) {
+            errorEmail = "Please Enter Valid Email Address";
+        }
+        if (errorEmail) {
+            this.setState({ errorEmail });
+            return false
+        }
+
+        if (this.state.subject.length <= 30) {
+            errorMessage = "Please Enter More Than 30 Characters";
+        }
+        if (errorMessage) {
+            this.setState({ errorMessage });
+
+            return false
+
+        }
+
+        return true;
+
+        ;
+
+    };
+
+    handleSubmit = event => {
+        const isValid = this.validate();
+        if (isValid) {
+            console.log(this.state);
+            alert("Form Submitted Successfully!")
+        }
+        else if (!isValid) {
+            event.preventDefault();
+
+        }
+    };
+
+    render() {
+
+        return (<div>
+          <div class="hero-image--contact">
+          <div class="hero-text--contact">
+              <h1>Contact Me</h1>
+              <p>Let's Connect.</p>
+              <br />
+              <span></span>
+              <span></span>
+              <span></span>
+          </div>
+      </div>
+            <form className="container-form" onSubmit={this.handleSubmit} action="https://formsubmit.co/davidgarcia1723@gmail.com" method='POST'>
+                <div className="formGroup">
+
+                    <label htmlFor="Name">Name</label>
+                    <br />
+                    <input type="text" id="Name" name="Name" value={this.state.Name} onChange={this.handleChange} placeholder="Your name.." />
+                    <div style={{ fontSize: 15, color: "red" }}>
+                        {this.state.errorName}
+                    </div>
+
+
+                    <label for="Email">E-mail</label>
+                    <br />
+                    <input type="text" id="Email" name="Email" value={this.state.Email} onChange={this.handleChange} placeholder="Your E-mail.." />
+                    <div style={{ fontSize: 15, color: "red" }}>
+                        {this.state.errorEmail}
+                    </div>
+
+
+                    <label for="Message">Message:</label>
+                    <br />
+                    <textarea id="Message" name="Message" value={this.state.subject} onChange={this.handleChange} placeholder="Write message.." style={{ height: '200px' }}></textarea>
+                    <div style={{ fontSize: 15, color: "red" }}>
+                        {this.state.errorMessage}
+                    </div>
+
+                    <button className="btn-submit" type="submit">Submit</button>
+                    
+                </div>
+
+            </form>
+            </div>
+
+        )
+    }
+}
+
+export default Contact
