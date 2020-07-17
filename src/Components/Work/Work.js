e7abefd21d361b3a554a7bb09074a2e5fcd07495
David Garcia <davidgarcia1723@gmail.com>
2020-07-17 14:55:04 -0400

    style update

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -34,6 +34,40 @@ class Work extends React.Component {
                         }
                     })}
                 </div>
+                <div className="card-container">
+                <div className="card">
+                        <article>
+                        <a href="https://github.com/davidgarcia1723/counter"><img src="/images/Counter.png" alt="counter" /></a>
+                        <h1>Node Weather App</h1>
+                        <p>Simple Counter app created using React lifecycles.</p>
+                        <div className="btn">
+                        <a href="https://counter-garcia.netlify.app/">Site</a>
+                        <a href="https://github.com/davidgarcia1723/counter">Github</a>
+                    </div>
+                    </article>
+                </div>
+                    <div className="card">
+                        <article>
+                        <a href="https://github.com/davidgarcia1723/weather-app"><img src="/images/Weather-app.png" alt="weatherapp" /></a>
+                        <h1>Node Weather App</h1>
+                        <p>Weather app created using Node.js and Express</p>
+                        <div className="btn">
+                        <a href="https://github.com/davidgarcia1723/weather-app">Github</a>
+                    </div>
+                    </article>
+                </div>
+                <div className="card">
+                        <article>
+                        <a href="https://github.com/davidgarcia1723/Pokemon"><img src="/images/Pokemon.jpg" alt="pokemonapp" /></a>
+                        <h1>Node Weather App</h1>
+                        <p>Pokemon inspired program created with Node and Inquirer.</p>
+                        <div className="btn">
+                        <a href="https://github.com/davidgarcia1723/Pokemon">Github</a>
+                    </div>
+                    </article>
+                </div>
+        
+                </div>
             </div>
         )
         }
